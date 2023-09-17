93d12197610b5aeb619cb78848b15d5ad03191aa
Alfredo <alfredoqooo@gmail.com>
2023-09-17 16:18:39 -0500

    boostrap

--- src/main/java/edu/pe/cibertec/app_semana4/Controller/FormImcController.java
+++ src/main/java/edu/pe/cibertec/app_semana4/Controller/FormImcController.java
@@ -1,4 +1,13 @@
 package edu.pe.cibertec.app_semana4.Controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class FormImcController {
+    @GetMapping("/promedionotas")
+    public String index(Model model){
+        return "formimc";
+    }
 }
--- src/main/java/edu/pe/cibertec/app_semana4/Model/PromedioModel.java
+++ src/main/java/edu/pe/cibertec/app_semana4/Model/PromedioModel.java
@@ -1,4 +1,7 @@
 package edu.pe.cibertec.app_semana4.Model;
 
 public class PromedioModel {
-}
+    private Integer cl1;
+    private Integer cl2;
+    private Integer cl3;
+    private Integer profin;}
