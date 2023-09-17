bbc2baeaf9b599dcbe24ef1a197b555360995416
Alfredo <alfredoqooo@gmail.com>
2023-09-17 14:02:12 -0500

    Cambio

---
+++ src/main/java/edu/pe/cibertec/app_semana4/AppSemana4Application.java
@@ -0,0 +1,13 @@
+package edu.pe.cibertec.app_semana4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppSemana4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppSemana4Application.class, args);
+	}
+
+}
---
+++ src/test/java/edu/pe/cibertec/app_semana4/AppSemana4ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.pe.cibertec.app_semana4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppSemana4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
